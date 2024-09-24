b66b86a96225d13d684a455bfe89d9dd495233f8
ricardoingeaa <115902704+ricardoingeaa@users.noreply.github.com>
2024-09-23 22:42:53 -0600

    prep w2

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+public class Job{
+
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+    public Job(){}
+
+    public void Display(){
+        Console.WriteLine($"{_jobTitle} ({_company} {_startYear}-{_endYear})");
+    }
+
+}
